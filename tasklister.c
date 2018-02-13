201f13e43927bda51ced8ad1bb1e193033522893
utsabab <utsab.khakurel@bison.howard.edu>
2018-02-12 23:23:22 -0700

    debugging last committed code

--- tasklister.c
+++ tasklister.c
@@ -11,13 +11,16 @@ static int task_lister_init(void) {
 
 	for_each_process(task) {
 		printk (KERN_INFO "pid: %d | pname: %s | state: %ld\n", task->pid, task->comm, task->state);
+		
+	
+		struct task_struct* current_state;
 		current_state = task->real_parent;
 
-		while (current_state != NULL) {
-			printk (KERN_INFO "pancestorid: %d | pancestorname | pancestorstate: %ld\n", current_state->pid, current_state->comm, current_state->state);
+		while (current_state->pid != 0) {
+			printk (KERN_INFO "pancestorid: %d | pancestorname %s | pancestorstate: %ld\n", current_state->pid, current_state->comm, current_state->state);
 			current_state = current_state->real_parent;
 		}
-		
+		printk (KERN_INFO "ID, Name and state printed for this process & their ancestors\n\n");
 	}
 
 	printk(KERN_INFO "MODULE LOADED\n");
