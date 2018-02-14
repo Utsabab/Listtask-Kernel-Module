e9a14d4ec466fb837ee38cc669ba2ee333592687
utsabab <utsab.khakurel@bison.howard.edu>
2018-02-13 22:08:36 -0700

    added comments to the code

--- tasklister.c
+++ tasklister.c
@@ -11,14 +11,17 @@ static int task_lister_init(void) {
 
 	for_each_process(task) {
 		printk (KERN_INFO "pid: %d | pname: %s | state: %ld\n", task->pid, task->comm, task->state);
+		//prints the pid, state and name of the current process
 		
 	
 		struct task_struct* current_state;
 		current_state = task->real_parent;
 
-		while (current_state->pid != 0) {
-			printk (KERN_INFO "pancestorid: %d | pancestorname %s | pancestorstate: %ld\n", current_state->pid, current_state->comm, current_state->state);
-			current_state = current_state->real_parent;
+		while (current_state->pid != 0) 
+		//while loop until the process reaches it root process		
+		{
+			printk (KERN_INFO "pancestorid: %d | pancestorname %s | pancestorstate: %ld\n", current_state->pid, current_state->comm, current_state->state);	//prints the pid, state and name of the nearest parent or current_state in this case
+			current_state = current_state->real_parent; //change the current_state to the next nearest parent 
 		}
 		printk (KERN_INFO "ID, Name and state printed for this process & their ancestors\n\n");
 	}
